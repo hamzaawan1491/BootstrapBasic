20ee7f781acfddbdbb681fdbef03080228d52d98
Hamza Awan <hamzaofficial3366@gmail.com>
2020-05-21 17:49:29 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+
+	const scroll = new SmoothScroll('.navbar a[href*="#"]',{
+		speed: 800
+	});
+
+	
+
+//FOR DIRECT ANIMATION
+// var scroll = new SmoothScroll();
+// var anchor = document.querySelector('#about');
+// scroll.animateScroll(anchor);
+
+
